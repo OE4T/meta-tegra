OE4T/meta-tegra

commit d377317bfc3bd3e9365602d91e3fdeb5960de07d

meta: generate extlinux.conf files in u-boot recipe
Use OE-Core's built-in support for generating U-Boot
extlinux.conf files, moving their generation to the
u-boot-tegra recipes. This fixes the circular dependency
problem mentioned in a79a9db89f876501a9dc3152def745c3ba0a4eaa
and reverts that change, so tegra186 and tegra210 images
again use an initrd.

Summary of changes:
 - add UBOOT_EXTLINUX_* variable settings to the
   u-boot-tegra recipes
 - remove extlinux.conf generation from kernel recipes
 - move the KERNEL_* variable settings that were used
   for generating the kernel command line in the
   extlinux.conf files into the machine/soc config files.
 - add 'u-boot' to MACHINE_ESSENTIAL_EXTRA_RDEPENDS when
   u-boot is the bootloader

Signed-off-by: Matt Madison <matt@madison.systems>

recipes-bsp/u-boot/u-boot-tegra_2016.07.bb
@@ -8,7 +8,7 @@ DEPENDS += "dtc-native ${SOC_FAMILY}-flashtools-native"
 CBOOTDEP = ""
 CBOOTDEP_tegra186 = "cboot:do_deploy virtual/kernel:do_deploy"
 
-DEPENDS_append_tegra186 = " tegra186-flashtools-native tegra-bootfiles nv-tegra-release dtc-native"
+DEPENDS_append_tegra186 = " tegra-bootfiles nv-tegra-release"
 
 inherit image_types_tegra
 
@@ -95,3 +95,5 @@ do_deploy_append_tegra186 () {
         ln -sf ${UBOOT_IMAGE}.bup-payload ${UBOOT_BINARY}.bup-payload
     fi
 }
+
+RPROVIDES_${PN} += "u-boot"

conf/machine/include/tegra-common.inc
@@ -2,6 +2,9 @@ MACHINEOVERRIDES =. "cuda:tegra:"
 SOC_FAMILY_PKGARCH = "${ARMPKGARCH}${ARMPKGSFX_DSP}${ARMPKGSFX_EABI}${ARMPKGSFX_ENDIAN}${ARMPKGSFX_FPU}_${SOC_FAMILY}"
 PACKAGE_EXTRA_ARCHS_append = " ${SOC_FAMILY_PKGARCH}"
 
+KERNEL_ROOTSPEC ?= "root=/dev/mmcblk${@uboot_var('devnum')}p${@uboot_var('distro_bootpart')} rw rootwait"
+KERNEL_ARGS ??= ""
+
 PREFERRED_PROVIDER_virtual/kernel ?= "linux-tegra"
 PREFERRED_PROVIDER_u-boot ?= "u-boot-tegra"
 PREFERRED_PROVIDER_virtual/bootloader ?= "u-boot-tegra"

conf/machine/include/tegra124.inc
@@ -8,9 +8,10 @@ require conf/machine/include/soc-family.inc
 require conf/machine/include/tune-cortexa15.inc
 
 KERNEL_IMAGETYPE = "zImage"
+KERNEL_ROOTSPEC ?= "root=/dev/mmcblk0p1 rw rootwait"
 
 MACHINE_FEATURES = "alsa usbhost pci rtc cuda"
-MACHINE_ESSENTIAL_EXTRA_RDEPENDS = "tegra-firmware kernel-devicetree kernel-image"
+MACHINE_ESSENTIAL_EXTRA_RDEPENDS = "tegra-firmware kernel-devicetree kernel-image u-boot"
 MACHINE_EXTRA_RDEPENDS = "tegra-configs-nvstartup tegra-configs-udev"
 
 SERIAL_CONSOLE = "115200 ttyS0"

conf/machine/include/tegra186.inc
@@ -8,12 +8,12 @@ require conf/machine/include/arm/arch-armv8a.inc
 KERNEL_IMAGETYPE = "Image"
 
 MACHINE_FEATURES = "alsa bluetooth usbhost pci wifi rtc cuda"
-MACHINE_ESSENTIAL_EXTRA_RDEPENDS = "tegra-firmware ${@'' if d.getVar('PREFERRED_PROVIDER_virtual/bootloader').startswith('cboot') else 'kernel-devicetree kernel-image'}"
+MACHINE_ESSENTIAL_EXTRA_RDEPENDS = "tegra-firmware ${@'' if d.getVar('PREFERRED_PROVIDER_virtual/bootloader').startswith('cboot') else 'kernel-devicetree kernel-image u-boot'}"
 MACHINE_EXTRA_RDEPENDS = "tegra-nvpmodel tegra-configs-nvstartup tegra-configs-udev"
 MACHINE_EXTRA_RRECOMMENDS += "tegra-firmware-brcm kernel-module-bcmdhd"
 
 INITRAMFS_IMAGE ?= "${@'' if d.getVar('PREFERRED_PROVIDER_virtual/bootloader').startswith('cboot') else 'tegra-minimal-initramfs'}"
-TEGRA_INITRAMFS_INITRD ?= "${@'' if d.getVar('PREFERRED_PROVIDER_virtual/bootloader').startswith('cboot') else '0'}"
+TEGRA_INITRAMFS_INITRD ?= "${@'' if d.getVar('PREFERRED_PROVIDER_virtual/bootloader').startswith('cboot') else '1'}"
 INITRD_IMAGE ?= "${@'tegra-minimal-initramfs' if d.getVar('PREFERRED_PROVIDER_virtual/bootloader').startswith('cboot') else ''}"
 INITRD_FSTYPES ?= "${@'cpio.gz.cboot cpio.gz.cboot.bup-payload' if d.getVar('PREFERRED_PROVIDER_virtual/bootloader').startswith('cboot') else '${INITRAMFS_FSTYPES}'}"
 IMAGE_CLASSES_append = "${@' image_types_cboot' if d.getVar('PREFERRED_PROVIDER_virtual/bootloader').startswith('cboot') else ''}"

conf/machine/include/tegra210.inc
@@ -8,12 +8,12 @@ require conf/machine/include/arm/arch-armv8a.inc
 KERNEL_IMAGETYPE = "Image"
 
 MACHINE_FEATURES = "alsa bluetooth usbhost pci wifi rtc cuda"
-MACHINE_ESSENTIAL_EXTRA_RDEPENDS = "tegra-firmware kernel-devicetree kernel-image"
+MACHINE_ESSENTIAL_EXTRA_RDEPENDS = "tegra-firmware kernel-devicetree kernel-image u-boot"
 MACHINE_EXTRA_RDEPENDS = "tegra-configs-nvstartup tegra-configs-udev"
 MACHINE_EXTRA_RRECOMMENDS = "tegra-firmware-brcm kernel-module-bcmdhd"
 
 INITRAMFS_IMAGE ?= "tegra-minimal-initramfs"
-TEGRA_INITRAMFS_INITRD ?= "0"
+TEGRA_INITRAMFS_INITRD ?= "1"
 
 TEGRA_AUDIO_DEVICE ?= "tegrahda"
 

conf/machine/jetson-tk1.conf
@@ -6,6 +6,8 @@ require conf/machine/include/tegra124.inc
 
 KERNEL_DEVICETREE ?= "tegra124-jetson_tk1-pm375-000-c00-00.dtb"
 
+KERNEL_ARGS = "console=ttyS0,115200 console=tty1 no_console_suspend=1 lp0_vec=2064@0xf46ff000 mem=2015M@2048M memtype=255 ddr_die=2048M@2048M section=256M pmuboard=0x0177:0x0000:0x02:0x43:0x00 tsec=32M@3913M otf_key=c75e5bb91eb3bd947560357b64422f85 usbcore.old_scheme_first=1 usbcore.autosuspend=-1 core_edp_mv=1150 core_edp_ma=4000 tegraid=40.1.1.0.0 debug_uartport=lsport,3 power_supply=Adapter audio_codec=rt5640 modem_id=0 android.kerneltype=normal fbcon=map:1 commchip_id=0 usb_port_owner_info=0 lane_owner_info=6 emc_max_dvfs=0 touch_id=0@0 board_info=0x0177:0x0000:0x02:0x43:0x00 gpt"
+
 MACHINE_FEATURES += "ext2 ext3 vfat"
 
 UBOOT_MACHINE = "jetson-tk1_defconfig"

recipes-bsp/u-boot/u-boot-tegra-common-2016.07.inc
@@ -10,3 +10,32 @@ SRCREV = "8a044b187228e60b454529e72c0610edd9d845bf"
 PV .= "+git${SRCPV}"
 
 S = "${WORKDIR}/git"
+
+KERNEL_ROOTSPEC ??= ""
+KERNEL_ARGS ??= ""
+
+def uboot_var(name):
+    return '${' + name + '}'
+
+UBOOT_EXTLINUX = "1"
+UBOOT_EXTLINUX_LABELS = "primary"
+UBOOT_EXTLINUX_DEFAULT_LABEL = "primary"
+UBOOT_EXTLINUX_KERNEL_IMAGE_primary = "../${KERNEL_IMAGETYPE}"
+UBOOT_EXTLINUX_FDTDIR_primary = ""
+UBOOT_EXTLINUX_FDT_primary = ""
+UBOOT_EXTLINUX_ROOT_primary = "${@uboot_var('cbootargs')} ${KERNEL_ROOTSPEC}"
+# Console setting comes from ${cbootargs}
+UBOOT_EXTLINUX_CONSOLE = ""
+UBOOT_EXTLINUX_KERNEL_ARGS_primary = "${KERNEL_ARGS}"
+UBOOT_EXTLINUX_INITRD_primary = "${@'../initrd' if d.getVar('INITRAMFS_IMAGE') != '' and d.getVar('TEGRA_INITRAMFS_INITRD') == '1' else ''}"
+
+do_install_append() {
+	install -m 0644 ${DEPLOY_DIR_IMAGE}/${KERNEL_IMAGETYPE} ${D}/boot/
+	if [ -n "${INITRAMFS_IMAGE}" -a "${TEGRA_INITRAMFS_INITRD}" = "1" ]; then
+		install -m 0644 ${DEPLOY_DIR_IMAGE}/${INITRAMFS_IMAGE}-${MACHINE}.cpio.gz ${D}/boot/initrd
+	fi
+}
+do_install[depends] += "${@'${INITRAMFS_IMAGE}:do_image_complete' if d.getVar('INITRAMFS_IMAGE') != '' and d.getVar('TEGRA_INITRAMFS_INITRD') == '1' else ''}"
+
+EXTLINUX_OVERRIDABLE_VARS = "MENU_DESCRIPTION ROOT KERNEL_IMAGE FDTDIR FDT KERNEL_ARGS INITRD"
+do_create_extlinux_config[vardeps] += "${@' '.join(['UBOOT_EXTLINUX_%s_%s' % (v, l) for v in d.getVar('EXTLINUX_OVERRIDABLE_VARS').split() for l in d.getVar('UBOOT_EXTLINUX_LABELS').split()])}"

recipes-bsp/u-boot/u-boot-tegra_2014.10.bb
@@ -19,6 +19,20 @@ PV .= "+git${SRCPV}"
 
 S = "${WORKDIR}/git"
 
+def uboot_var(name):
+    return '${' + name + '}'
+
+UBOOT_EXTLINUX = "1"
+UBOOT_EXTLINUX_LABELS = "primary"
+UBOOT_EXTLINUX_DEFAULT_LABEL = "primary"
+UBOOT_EXTLINUX_KERNEL_IMAGE_primary = "../${KERNEL_IMAGETYPE}"
+UBOOT_EXTLINUX_FDTDIR_primary = ""
+UBOOT_EXTLINUX_FDT_primary = "../${@d.getVar('KERNEL_DEVICETREE').split()[0]}"
+UBOOT_EXTLINUX_ROOT_primary = "${KERNEL_ROOTSPEC}"
+# Set in KERNEL_ARGS for ordering
+UBOOT_EXTLINUX_CONSOLE = ""
+UBOOT_EXTLINUX_KERNEL_ARGS_primary = "${KERNEL_ARGS}"
+
 do_configure() {
     if [ -z "${UBOOT_CONFIG}" ]; then
 	if [ -n "${UBOOT_MACHINE}" ]; then
@@ -30,3 +44,8 @@ do_configure() {
 	cml1_do_configure
     fi
 }
+
+EXTLINUX_OVERRIDABLE_VARS = "MENU_DESCRIPTION ROOT KERNEL_IMAGE FDTDIR FDT KERNEL_ARGS INITRD"
+do_create_extlinux_config[vardeps] += "${@' '.join(['UBOOT_EXTLINUX_%s_%s' % (v, l) for v in d.getVar('EXTLINUX_OVERRIDABLE_VARS').split() for l in d.getVar('UBOOT_EXTLINUX_LABELS').split()])}"
+
+RPROVIDES_${PN} += "u-boot"

commit f4f50b42ee992d62337af772af20dadd2cf14620

Fix u-boot-fw-utils build
Broken with changes in d377317bfc3bd3e9365602d91e3fdeb5960de07d where
u-boot specific install steps were placed in common file shared with
fw-utils-tegra.  Fix naming of u-boot-common file and add a note that
this is shared with fw-utils.

recipes-bsp/u-boot/u-boot-common-tegra_2016.07.inc
@@ -1,3 +1,4 @@
+# This file is shared with fw-utils and u-boot builds
 LICENSE = "GPLv2+"
 DESCRIPTION = "U-Boot for Nvidia Tegra platforms, based on Nvidia sources"
 COMPATIBLE_MACHINE = "(tegra186|tegra210)"
@@ -28,10 +29,3 @@ UBOOT_EXTLINUX_ROOT_primary = "${@uboot_var('cbootargs')} ${KERNEL_ROOTSPEC}"
 UBOOT_EXTLINUX_CONSOLE = ""
 UBOOT_EXTLINUX_KERNEL_ARGS_primary = "${KERNEL_ARGS}"
 UBOOT_EXTLINUX_INITRD_primary = "${@'../initrd' if d.getVar('INITRAMFS_IMAGE') != '' and d.getVar('TEGRA_INITRAMFS_INITRD') == '1' else ''}"
-
-do_install_append() {
-    if [ -n "${INITRAMFS_IMAGE}" -a "${TEGRA_INITRAMFS_INITRD}" = "1" ]; then
-        install -m 0644 ${DEPLOY_DIR_IMAGE}/${INITRAMFS_IMAGE}-${MACHINE}.cpio.gz ${D}/boot/initrd
-    fi
-}
-do_install[depends] += "${@'${INITRAMFS_IMAGE}:do_image_complete' if d.getVar('INITRAMFS_IMAGE') != '' and d.getVar('TEGRA_INITRAMFS_INITRD') == '1' else ''}"

recipes-bsp/u-boot/u-boot-fw-utils-tegra_2016.07.bb
@@ -1,4 +1,4 @@
-require u-boot-tegra-common-${PV}.inc
+require u-boot-common-tegra_${PV}.inc
 
 SUMMARY = "U-Boot bootloader fw_printenv/setenv utilities"
 DEPENDS = "mtd-utils"

recipes-bsp/u-boot/u-boot-tegra_2016.07.bb
@@ -1,7 +1,7 @@
 UBOOT_BINARY ?= "u-boot-dtb.${UBOOT_SUFFIX}"
 
 require recipes-bsp/u-boot/u-boot.inc
-require u-boot-tegra-common-${PV}.inc
+require u-boot-common-tegra_${PV}.inc
 
 PROVIDES += "u-boot"
 DEPENDS += "dtc-native bc-native ${SOC_FAMILY}-flashtools-native tegra-bootfiles nv-tegra-release"
@@ -95,5 +95,12 @@ do_deploy_append_tegra186 () {
         ln -sf ${UBOOT_IMAGE}.bup-payload ${UBOOT_BINARY}.bup-payload
     fi
 }
+
+do_install_append() {
+    if [ -n "${INITRAMFS_IMAGE}" -a "${TEGRA_INITRAMFS_INITRD}" = "1" ]; then
+        install -m 0644 ${DEPLOY_DIR_IMAGE}/${INITRAMFS_IMAGE}-${MACHINE}.cpio.gz ${D}/boot/initrd
+    fi
+}
+do_install[depends] += "${@'${INITRAMFS_IMAGE}:do_image_complete' if d.getVar('INITRAMFS_IMAGE') != '' and d.getVar('TEGRA_INITRAMFS_INITRD') == '1' else ''}"
 
 RPROVIDES_${PN} += "u-boot"