471718cefb9ff6b7d9aa78887021ce68f2ca0ba0
Matt Madison <matt@madison.systems>
2020-02-08 09:44:41 -0800

    libusbgx, libusbgx-config: add bbappends, L4T USB device mode schema
    For enabling a more OE-friendly equivalent of the
    l4t-usb-device-mode configuration settings from NVIDIA.
    
    The schema here only defines the ACM, ECM, and RNDIS gadgets;
    others could be added.
    
    Signed-off-by: Matt Madison <matt@madison.systems>

---
+++ external/openembedded-layer/recipes-support/libusbgx/libusbgx-config/l4t-gadget-config-setup.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+[ -d /run/usbgx ] || mkdir /run/usbgx
+[ ! -e /run/usbgx/l4t.schema ] || exit 0
+if [ ! -e /usr/share/usbgx/l4t.schema.in ]; then
+    echo "ERR: missing gadget schema template" >&2
+    exit 1
+fi
+sernum=$(cat /proc/device-tree/serial-number 2>/dev/null | tr -d '\000')
+[ -n "$sernum" ] || sernum="UNKNOWN"
+sed -e"s,@SERIALNUMBER@,$sernum," /usr/share/usbgx/l4t.schema.in > /run/usbgx/l4t.schema
+chmod 0644 /run/usbgx/l4t.schema
