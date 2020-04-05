0cde60dfd22455f3cb12366cee563629ccb22de1
Matt Madison <matt@madison.systems>
2020-04-05 04:58:39 -0700

    tegra-helper-scripts: update tegra210-flash-helper for signing
    Signing functions in odmsign.func take a different path for
    tegra210 SoCs than for the others, which requires some additional
    variable settings and post-processing in the helper script.
    
    Signed-off-by: Matt Madison <matt@madison.systems>

--- recipes-bsp/tegra-binaries/tegra-helper-scripts/tegra210-flash-helper.sh
+++ recipes-bsp/tegra-binaries/tegra-helper-scripts/tegra210-flash-helper.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 bup_build=
 keyfile=
-no_flash=
+no_flash=0
 sdcard=
 make_sdcard_args=
 imgfile=
@@ -21,7 +21,7 @@ while true; do
 	    shift
 	    ;;
 	--no-flash)
-	    no_flash=yes
+	    no_flash=1
 	    shift
 	    ;;
 	--sdcard)
@@ -149,7 +149,7 @@ date "+%Y%m%d%H%M%S" >>${MACHINE}_bootblob_ver.txt
 bytes=`cksum ${MACHINE}_bootblob_ver.txt | cut -d' ' -f2`
 cksum=`cksum ${MACHINE}_bootblob_ver.txt | cut -d' ' -f1`
 echo "BYTES:$bytes CRC32:$cksum" >>${MACHINE}_bootblob_ver.txt
-if [ -z "$bup_build" ]; then
+if [ -z "$bup_build" -a $no_flash -eq 0 ]; then
     if [ -z "$sdcard" ]; then
 	appfile=$(echo $(basename "$imgfile") | cut -d. -f1).img
     else
@@ -166,29 +166,39 @@ if [ "$bup_build" = "yes" -o "$sdcard" = "yes" ]; then
     cmd="sign"
     binargs=
 else
-    if [ -z "$sdcard" ]; then
+    if [ -z "$sdcard" -a $no_flash -eq 0 ]; then
 	rm -f "$appfile"
 	$here/mksparse -b ${blocksize} -v --fillpattern=0 "$imgfile"  "$appfile" || exit 1
     fi
     if [ -n "$keyfile" ]; then
+	dbmaster=$(readlink -f "$keyfile")
+	fusetype="PKC"
 	CHIPID="0x21"
 	tegraid="$CHIPID"
 	localcfgfile="flash.xml"
 	BINSARGS=
+	DTBARGS="--bldtb $dtb_file "
+	SOSARGS=" --applet nvtboot_recovery.bin "
 	dtbfilename="$dtb_file"
 	localbootfile="$kernfile"
 	flashername="cboot.bin"
+	bootloadername="cboot.bin"
 	BCT="--bct"
 	bctfilename="$sdramcfg_file"
+	flashappname=$(basename "$flashapp")
 	. "$here/odmsign.func"
 	odmsign_ext || exit 1
-	if [ -n "$no_flash" ]; then
-	    if [ -f flashcmd.txt ]; then
-		chmod +x flashcmd.txt
-		cp flashcmd.txt ./secureflash.sh
-	    else
-		echo "WARN: signing completed successfully, but flashcmd.txt missing" >&2
+	if [ $no_flash -ne 0 ]; then
+	    rm -f flashcmd.txt
+	    echo "#!/bin/sh" > flashcmd.txt
+	    if [ "$boardid" = "3448" ]; then
+		binargs="--bins \"EBT cboot.bin.signed;DTB ${dtb_file}.signed\""
 	    fi
+	    echo "python3 $flashapp --bl cboot.bin.signed --bct \"$sdramcfg_file\" --odmdata $odmdata \
+--bldtb \"${dtb_file}.signed\" --applet rcm_1_signed.rcm --cfg flash.xml --chip 0x21 \
+--cmd \"secureflash;reboot\" $binargs" > flashcmd.txt
+	    chmod +x flashcmd.txt
+	    ln -sf flashcmd.txt ./secureflash.sh
 	fi
 	exit 0
     else
