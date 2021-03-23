40701c99c42563a871b8fca0116440f0958a1124
Matt Madison <matt@madison.systems>
2021-03-23 03:59:20 -0700

    tegraXXX-helper-scripts: add TNSPEC length check
    Generate an error during BUP generation if the TNSPEC
    will be longer than 63 characters, since the tools that
    deal with BUPs have a hard limit on that length.
    
    Signed-off-by: Matt Madison <matt@madison.systems>

--- recipes-bsp/tegra-binaries/tegra-helper-scripts/tegra186-flash-helper.sh
+++ recipes-bsp/tegra-binaries/tegra-helper-scripts/tegra186-flash-helper.sh
@@ -318,6 +318,10 @@ if [ $bup_blob -ne 0 ]; then
     localbootfile="boot.img"
     . "$here/l4t_bup_gen.func"
     spec="${BOARDID}-${FAB}-${BOARDSKU}--1-0-${MACHINE}-${BOOTDEV}"
+    if [ $(expr length "$spec") -ge 64 ]; then
+	echo "ERR: TNSPEC must be shorter than 64 characters: $spec" >&2
+	exit 1
+    fi
     l4t_bup_gen "$flashcmd" "$spec" "$fuselevel" t186ref "$keyfile" "$sbk_keyfile" 0x18 || exit 1
 else
     eval $flashcmd || exit 1
