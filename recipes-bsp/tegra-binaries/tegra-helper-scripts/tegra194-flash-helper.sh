6b07ef35d94f386f4068617e93dc25808e589e3c
Matt Madison <matt@madison.systems>
2021-10-02 04:03:47 -0700

    tegra-helper-scripts: set no_flash for BUP generation
    as well as setting bup_blob, so the odmsign functions behave
    properly when doing PKC-only signing for t186/t194 platforms.
    Change the t210 script to match, and fix an issue in the PT
    generator function so it works when run under pseudo during
    a build.
    
    Signed-off-by: Matt Madison <matt@madison.systems>

--- recipes-bsp/tegra-binaries/tegra-helper-scripts/tegra194-flash-helper.sh
+++ recipes-bsp/tegra-binaries/tegra-helper-scripts/tegra194-flash-helper.sh
@@ -23,6 +23,7 @@ while true; do
     case "$1" in
 	--bup)
 	    bup_blob=1
+	    no_flash=1
 	    shift
 	    ;;
 	--no-flash)
@@ -349,7 +350,7 @@ if [ -n "$keyfile" ]; then
     BCTARGS="$bctargs"
     . "$here/odmsign.func"
     (odmsign_ext) || exit 1
-    if [ $no_flash -ne 0 ]; then
+    if [ $bup_blob -eq 0 -a $no_flash -ne 0 ]; then
 	if [ -f flashcmd.txt ]; then
 	    chmod +x flashcmd.txt
 	    ln -sf flashcmd.txt ./secureflash.sh
