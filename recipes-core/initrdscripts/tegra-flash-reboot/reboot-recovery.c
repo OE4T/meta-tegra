6475b97db5558dfd2c7fc8eeaa5f249ff8412fbb
Matt Madison <matt@madison.systems>
2022-12-14 14:12:23 -0800

    tegra-flash-reboot: add recipe
    to provide a small utility to reboot into recovery mode,
    which can't be done with busybox's reboot command.
    
    Signed-off-by: Matt Madison <matt@madison.systems>

---
+++ recipes-core/initrdscripts/tegra-flash-reboot/reboot-recovery.c
@@ -0,0 +1,16 @@
+/* SPDX-License-Identifier: MIT */
+#include <linux/reboot.h>
+#include <sys/reboot.h>
+#include <sys/syscall.h>
+
+static inline int sys_reboot(const void *arg) {
+	return (int) syscall(SYS_reboot, LINUX_REBOOT_MAGIC1, LINUX_REBOOT_MAGIC2, LINUX_REBOOT_CMD_RESTART2, arg);
+}
+
+int
+main (int argc, char *argv[])
+{
+	if (argc > 1)
+		return sys_reboot(argv[1]);
+	return sys_reboot("forced-recovery");
+}
