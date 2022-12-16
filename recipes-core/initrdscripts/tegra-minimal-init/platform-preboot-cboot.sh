4349c0aad1aa94f0575f50f892146dd9570290a3
Matt Madison <matt@madison.systems>
2022-12-16 04:13:57 -0800

    tegra-minimal-init: add retries for locating APP partition label
    When booting off a USB storage device, we need to give the kernel time
    to probe the device.
    
    Signed-off-by: Matt Madison <matt@madison.systems>

--- recipes-core/initrdscripts/tegra-minimal-init/platform-preboot-cboot.sh
+++ recipes-core/initrdscripts/tegra-minimal-init/platform-preboot-cboot.sh
@@ -8,8 +8,20 @@ for bootarg in `cat /proc/cmdline`; do
 	rootwait) wait="yes" ;;
     esac
 done
-rootdev=`blkid -l -t PARTLABEL=APP$slotsfx | cut -d: -f1`
+message="Waiting for APP$slotsfx partition..."
+for count in $(seq 1 10); do
+    blkid --probe 2>/dev/null
+    rootdev=`blkid -l -t PARTLABEL=APP$slotsfx | cut -d: -f1`
+    if [ -n "$rootdev" ]; then
+	echo "[OK: $rootdev]"
+	break
+    fi
+    echo -n "$message"
+    message="."
+    sleep 0.5
+done
 if [ -z "$rootdev" ]; then
+    echo "[FAIL]"
     if [ -n "$mayberoot" ]; then
 	rootdev="$mayberoot"
     else
