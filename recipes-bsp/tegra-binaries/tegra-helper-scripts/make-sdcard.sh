e6d0d29cb0b85f07a304b8dd243399a20cab6f8a
Matt Madison <matt@madison.systems>
2022-12-30 09:06:39 -0800

    tegra-helper-scripts: improve make-sdcard handling of internal storage
    Added an option to honor the starting locations taken from the XML file.
    
    Also:
    * Updated progress output to be a bit more informative
    * Added help text for other recently-added options
    
    Signed-off-by: Matt Madison <matt@madison.systems>

--- recipes-bsp/tegra-binaries/tegra-helper-scripts/make-sdcard.sh
+++ recipes-bsp/tegra-binaries/tegra-helper-scripts/make-sdcard.sh
@@ -21,15 +21,18 @@ Usage:
   $me [options] [--] <config-file> <output>
 
 Parameters:
-  config-file           Name of the flash.xml file with the SDcard partition definitions
-  output                Either the name of an SDcard device, or the name of an SDcard image file to create
+  config-file             Name of the flash.xml file with the SDcard partition definitions
+  output                  Either the name of an SDcard device, or the name of an SDcard image file to create
 
 Options:
-  -h                    Displays this usage information
-  -s size               Sets size of SDcard image when creating an image file (required)
-  -b basename           Base filename for SDcard image (required if no output specified)
-  -y                    Skip prompting for confirmation
-  --serial-number <sn>  Select USB /dev/sd[a-z] device based on serial number
+  -h                      Displays this usage information
+  -s size                 Sets size of SDcard image when creating an image file (required)
+  -b basename             Base filename for SDcard image (required if no output specified)
+  -y                      Skip prompting for confirmation
+  --honor-start-locations Use the start locations emitted by nvflashxmlparse
+  --no-final-part         Skip special handling of final partition
+  --serial-number <sn>    Select USB /dev/sd[a-z] device based on serial number
+  --keep-connection       Do not disconnect USB drive after use
 
 Confirmation is required if <output> is a device or if it is the name of
 a file that already exists.
@@ -92,25 +95,35 @@ find_finalpart() {
 }
 
 make_partitions() {
-    local blksize partnumber partname partsize partfile partguid partfilltoend
-    local i pline
+    local blksize partnumber partname partsize partfile partguid partfilltoend start_location
+    local i pline alignarg
+    if [ "$use_start_locations" = "yes" ]; then
+	alignarg="-a 1"
+    fi
     i=0
     for pline in "${PARTS[@]}"; do
 	if [ $i -ne $FINALPART ]; then
 	    eval "$pline"
-	    echo -n "$partname..."
-	    sgdisk "$output" --new=$partnumber:0:+$partsize --typecode=$partnumber:8300 -c $partnumber:$partname >/dev/null 2>&1
+	    if [ "$use_start_locations" != "yes" ]; then
+		start_location=0
+	    fi
+	    printf "  [%02d] name=%s start=%s size=%s sectors\n" $partnumber $partname $start_location $partsize
+	    sgdisk "$output" $alignarg --new=$partnumber:$start_location:+$partsize --typecode=$partnumber:8300 -c $partnumber:$partname >/dev/null 2>&1
 	fi
 	i=$(expr $i + 1)
     done
     if [ -z "$ignore_finalpart" ]; then
 	eval "${PARTS[$FINALPART]}"
 	[ -n "$parttype" ] || parttype="8300"
-	echo -n "$partname..."
+	if [ "$use_start_locations" != "yes" ]; then
+	    start_location=0
+	fi
 	if [ $partfilltoend -eq 1 ]; then
-	    sgdisk "$output" -a 8 --largest-new=$partnumber --typecode=$partnumber:$parttype -c $partnumber:$partname >/dev/null 2>&1
+	    printf "  [%02d] name=%s (fills to end)\n" $partnumber $partname
+	    sgdisk "$output" $alignarg --largest-new=$partnumber --typecode=$partnumber:$parttype -c $partnumber:$partname >/dev/null 2>&1
 	else
-	    sgdisk "$output" -a 8 --new=$partnumber:0:+$partsize --typecode=$partnumber:$parttype -c $partnumber:$partname >/dev/null 2>&1
+	    printf "  [%02d] name=%s start=%s size=%s sectors\n" $partnumber $partname $start_location $partsize
+	    sgdisk "$output" $alignarg --new=$partnumber:$start_location:+$partsize --typecode=$partnumber:$parttype -c $partnumber:$partname >/dev/null 2>&1
 	fi
     fi
 }
@@ -134,7 +147,7 @@ copy_to_device() {
 
 write_partitions_to_device() {
     local blksize partnumber partname partsize partfile partguid partfilltoend
-    local i dest pline
+    local i dest pline destsize filesize
     i=0
     for pline in "${PARTS[@]}"; do
 	if [ $i -eq $FINALPART ]; then
@@ -152,12 +165,14 @@ write_partitions_to_device() {
 	    echo "ERR: cannot find file $partfile for partition $partnumber" >&2
 	    return 1
 	fi
+	filesize=$(stat -c "%s" "$partfile")
 	dest="/dev/$DEVNAME$PARTSEP$partnumber"
 	if [ ! -b "$dest" ]; then
 	    echo "ERR: cannot locate block device $dest" >&2
 	    return 1
 	fi
-	echo -n "$partname..."
+	destsize=$(blockdev --getsize64 "$dest")
+	echo "  Writing $partfile (size=$filesize) to $dest (size=$destsize)..."
 	if ! copy_to_device "$partfile" "$dest"; then
 	    echo "ERR: failed to write $partfile to $dest" >&2
 	    return 1
@@ -173,12 +188,14 @@ write_partitions_to_device() {
 	    echo "ERR: cannot find file $partfile for partition $partnumber" >&2
 	    return 1
 	fi
+	filesize=$(stat -c "%s" "$partfile")
 	dest="/dev/$DEVNAME$PARTSEP$partnumber"
 	if [ ! -b "$dest" ]; then
 	    echo "ERR: cannot locate block device $dest" >&2
 	    return 1
 	fi
-	echo -n "$partname..."
+	destsize=$(blockdev --getsize64 "$dest")
+	echo "  Writing $partfile (size=$filesize) to $dest (size=$destsize)..."
 	if ! copy_to_device "$partfile" "$dest"; then
 	    echo "ERR: failed to write $partfile to $dest" >&2
 	    return 1
@@ -205,7 +222,7 @@ write_partitions_to_image() {
 	    echo "ERR: cannot find file $partfile for partition $partnumber" >&2
 	    return 1
 	fi
-	echo -n "$partname..."
+	echo "  Writing $partfile..."
 	if ! dd if="$partfile" of="$output" conv=notrunc seek=${partstart[$partnumber]} status=none >/dev/null 2>&1; then
 	    echo "ERR: failed to write $partfile to $output (offset ${partstart[$partnumber]}" >&2
 	    return 1
@@ -234,7 +251,7 @@ confirm() {
     done
 }
 
-ARGS=$(getopt -l "serial-number:,keep-connection,no-final-part" -o "yhs:b:" -n "$me" -- "$@")
+ARGS=$(getopt -l "serial-number:,keep-connection,no-final-part,honor-start-locations" -o "yhs:b:" -n "$me" -- "$@")
 if [ $? -ne 0 ]; then
     usage
     exit 1
@@ -250,6 +267,7 @@ wait_for_usb_device=
 keep_connection=
 serial_number=
 ignore_finalpart=
+use_start_locations=
 while true; do
     case "$1" in
 	--serial-number)
@@ -263,6 +281,10 @@ while true; do
 	    ;;
 	--no-final-part)
 	    ignore_finalpart=yes
+	    shift
+	    ;;
+	--honor-start-locations)
+	    use_start_locations=yes
 	    shift
 	    ;;
 	-h)
@@ -367,7 +389,7 @@ if [ ${#PARTS[@]} -eq 0 ]; then
     exit 1
 fi
 
-echo -n "Init..."
+echo  "Creating partitions"
 [ -b "$output" ] || dd if=/dev/zero of="$output" bs=512 count=0 seek=$outsize status=none
 if ! sgdisk "$output" --clear --mbrtogpt >/dev/null 2>&1; then
     echo "ERR: could not initialize GPT on $output" >&2
@@ -376,7 +398,6 @@ fi
 
 find_finalpart || exit 1
 make_partitions || exit 1
-echo "[OK]"
 if ! sgdisk "$output" --verify >/dev/null 2>&1; then
     echo "ERR: verification failed for $output" >&2
     exit 1
@@ -392,7 +413,7 @@ fi
 if type -p bmaptool >/dev/null 2>&1; then
     HAVEBMAPTOOL=yes
 fi
-echo -n "Writing..."
+echo "Writing partitions"
 if [ -b "$output" ]; then
     write_partitions_to_device || exit 1
 else
