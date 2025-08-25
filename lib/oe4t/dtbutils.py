f646ea19f02d8f473a18e2c1f960fe30adb2e529
Dan Walkes <danwalkes@trellis-logic.com>
2025-08-25 09:36:31 -0600

    dtbutils: refactor library functions
    Bring locate_dtb_files out to a function which can be used for
    other purposes, including to copy dtb files instead of only to
    apply overlays.
    
    Signed-off-by: Dan Walkes <danwalkes@trellis-logic.com>

--- lib/oe4t/dtbutils.py
+++ lib/oe4t/dtbutils.py
@@ -1,3 +1,35 @@
+
+def locate_dtb_files(dtbnames, d):
+    """
+    Locate the full path of dtb files given
+    a list of names and the relevant bitbake
+    variables used for dtb locations.
+    dtbnames: Names of dtb files to search for
+    return: a list of full paths to dtb files
+    matching dtbnames
+    """
+
+    def find_file_under(fname, rootdir):
+        for dirname, _, f in os.walk(rootdir):
+            if fname in f:
+                return os.path.join(dirname, fname)
+        return None
+
+    result = []
+    extern_root = d.getVar('EXTERNAL_KERNEL_DEVICETREE')
+    imgdeploydir = d.getVar('DEPLOY_DIR_IMAGE')
+    for dtb in dtbnames:
+        if os.path.isabs(dtb):
+            result.append(dtb)
+            continue
+        if extern_root:
+            dtbpath = find_file_under(dtb, extern_root)
+            if dtbpath:
+                result.append(dtbpath)
+                continue
+        result.append(os.path.join(imgdeploydir, dtb))
+    return result
+
 def concat_dtb_overlays(dtbfile, overlays, outfile, d):
     """
     Produce an output DTB file that has zero or more
@@ -15,29 +47,8 @@ def concat_dtb_overlays(dtbfile, overlays, outfile, d):
     import os
     import bb.utils
 
-    def find_file_under(fname, rootdir):
-        for dirname, _, f in os.walk(rootdir):
-            if fname in f:
-                return os.path.join(dirname, fname)
-        return None
 
-    def locate_dtb_files(dtbnames):
-        result = []
-        extern_root = d.getVar('EXTERNAL_KERNEL_DEVICETREE')
-        imgdeploydir = d.getVar('DEPLOY_DIR_IMAGE')
-        for dtb in dtbnames:
-            if os.path.isabs(dtb):
-                result.append(dtb)
-                continue
-            if extern_root:
-                dtbpath = find_file_under(dtb, extern_root)
-                if dtbpath:
-                    result.append(dtbpath)
-                    continue
-            result.append(os.path.join(imgdeploydir, dtb))
-        return result
-
-    infiles = locate_dtb_files([dtbfile] + overlays.split())
+    infiles = locate_dtb_files([dtbfile] + overlays.split(), d)
     bb.note("Creating concatenated device tree: ", outfile)
     with open(outfile, "wb") as outf:
         for infile in infiles:
