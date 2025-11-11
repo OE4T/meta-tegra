e224bc66c4f47073a9cb7cb34891244f7a79c1c8
Matt Madison <matt@madison.systems>
2025-11-11 11:24:05 -0800

    cuda-compatibility-workarounds: update for CUDA 13
    The only outstanding compatbility issue CUDA 13 has with glibc 2.41+
    is the conflicting definition of some floating point functions.
    Retain the undef for that, and just use include_next to include
    the stock math-vector support.
    
    Signed-off-by: Matt Madison <matt@madison.systems>

--- recipes-devtools/cuda/cuda-compatibility-workarounds/math-vector.h
+++ recipes-devtools/cuda/cuda-compatibility-workarounds/math-vector.h
@@ -1,4 +1,2 @@
-#include <bits/libm-simd-decl-stubs.h>
-#undef __ADVSIMD_VEC_MATH_SUPPORTED
-#undef __SVE_VEC_MATH_SUPPORTED
 #undef __GLIBC_USE_IEC_60559_FUNCS_EXT_C23
+#include_next <bits/math-vector.h>
