madisongh/meta-tegra-wip

uefi: update r36.4.4 -> r36.5
* Configuration system changes like in r38.4.0, no
  more simple full/minimal toggle
* Drop patch that has made it upstream, refresh the others

Signed-off-by: Matt Madison <matt@madison.systems>

recipes-bsp/uefi/edk2-firmware-tegra-rcmboot_36.5.0.bb
@@ -1,20 +1,22 @@
-require edk2-firmware-tegra-36.4.4.inc
+require edk2-firmware-tegra-36.5.0.inc
 
-DESCRIPTION = "UEFI EDK2 Minimal Firmware for Jetson platforms"
+DESCRIPTION = "UEFI EDK2 Minimal Firmware for flashing Jetson platforms"
 
 TEGRA_UEFI_SIGNING_CLASS ??= "tegra-uefi-signing"
 
 inherit l4t_bsp deploy ${TEGRA_UEFI_SIGNING_CLASS}
 
-EDK2_PLATFORM = "JetsonMinimal"
-TEGRA_UEFI_MINIMAL = "1"
 EDK2_PLATFORM_DSC = "Platform/NVIDIA/NVIDIA.common.dsc"
-EDK2_BIN_NAME = "uefi_jetson_minimal.bin"
+TEGRA_EDK2_PLATFORM ??= "UNKNOWN"
+TEGRA_EDK2_CONFIGURATION = "embedded"
+EDK2_PLATFORM = "${TEGRA_EDK2_PLATFORM}_${TEGRA_EDK2_CONFIGURATION}"
+TEGRA_FLASHVAR_UEFI_IMAGE ??= "uefi_${EDK2_PLATFORM}"
+EDK2_BIN_NAME = "uefi_${EDK2_PLATFORM}.bin"
 
 SRC_URI += "file://nvbuildconfig.py"
 
 do_configure:append() {
-    ${PYTHON} ${UNPACKDIR}/nvbuildconfig.py --kconfig-path=${S_EDK2_NVIDIA}/Platform/NVIDIA/Kconfig --output-dir=${B}/nvidia-config/Jetson ${S_EDK2_NVIDIA}/Platform/NVIDIA/${EDK2_PLATFORM}/Jetson.defconfig ${@config_fragments(d)}
+    ${PYTHON} ${UNPACKDIR}/nvbuildconfig.py --kconfig-path=${S_EDK2_NVIDIA}/Platform/NVIDIA/Kconfig --output-dir=${B}/nvidia-config/Tegra/${EDK2_PLATFORM} ${S_EDK2_NVIDIA}/Platform/NVIDIA/Tegra/DefConfigs/${EDK2_PLATFORM}.defconfig ${@config_fragments(d)}
 }
 
 do_compile:append() {
@@ -32,7 +34,7 @@ do_install() {
 
 do_deploy() {
     install -d ${DEPLOYDIR}
-    install -m 0644 ${B}/images/${EDK2_BIN_NAME} ${DEPLOYDIR}/
+    install -m 0644 ${B}/images/${EDK2_BIN_NAME} ${DEPLOYDIR}/${TEGRA_FLASHVAR_RCM_UEFI_IMAGE}.bin
 }
 # Downstream consumers will need the dtb overlays created by the
 # normal build

recipes-bsp/uefi/edk2-firmware-tegra_36.5.0.bb
@@ -1,4 +1,4 @@
-require edk2-firmware-tegra-36.4.4.inc
+require edk2-firmware-tegra-36.5.0.inc
 
 DESCRIPTION = "UEFI EDK2 Firmware for Jetson platforms"
 
@@ -10,14 +10,17 @@ TEGRA_UEFI_SIGNING_CLASS ??= "tegra-uefi-signing"
 
 inherit l4t_bsp deploy ${TEGRA_UEFI_SIGNING_CLASS}
 
-EDK2_PLATFORM = "${@'JetsonMinimal' if bb.utils.to_boolean(d.getVar('TEGRA_UEFI_MINIMAL')) else 'Jetson'}"
 EDK2_PLATFORM_DSC = "Platform/NVIDIA/NVIDIA.common.dsc"
-EDK2_BIN_NAME = "uefi_jetson.bin"
+TEGRA_EDK2_PLATFORM ??= "UNKNOWN"
+TEGRA_EDK2_CONFIGURATION ??= "general"
+EDK2_PLATFORM = "${TEGRA_EDK2_PLATFORM}_${TEGRA_EDK2_CONFIGURATION}"
+TEGRA_FLASHVAR_UEFI_IMAGE ??= "uefi_${EDK2_PLATFORM}"
+EDK2_BIN_NAME = "uefi_${EDK2_PLATFORM}.bin"
 
 SRC_URI += "file://nvbuildconfig.py"
 
 do_configure:append() {
-    ${PYTHON} ${UNPACKDIR}/nvbuildconfig.py --kconfig-path=${S_EDK2_NVIDIA}/Platform/NVIDIA/Kconfig --output-dir=${B}/nvidia-config/Jetson ${S_EDK2_NVIDIA}/Platform/NVIDIA/${EDK2_PLATFORM}/Jetson.defconfig ${@config_fragments(d)}
+    ${PYTHON} ${UNPACKDIR}/nvbuildconfig.py --kconfig-path=${S_EDK2_NVIDIA}/Platform/NVIDIA/Kconfig --output-dir=${B}/nvidia-config/Tegra/${EDK2_PLATFORM} ${S_EDK2_NVIDIA}/Platform/NVIDIA/Tegra/DefConfigs/${EDK2_PLATFORM}.defconfig ${@config_fragments(d)}
 }
 
 def fmp_lowest_version(d):
@@ -30,18 +33,36 @@ def fmp_lowest_version(d):
     return "0x%02x%02x%02x%02x" % (branch_high, branch_low, major, minor)
 
 do_compile:append() {
+
+    PATH="${WORKSPACE}:${BTOOLS_PATH}:$PATH" \
+    build \
+       --arch "${EDK2_ARCH}" \
+       --buildtarget ${EDK2_BUILD_MODE} \
+       --tagname ${EDK_COMPILER} \
+       --platform ${S_EDK2_NVIDIA}/Platform/NVIDIA/L4TLauncher/L4TLauncher.dsc \
+       ${@oe.utils.parallel_make_argument(d, "-n %d")} \
+       ${EDK2_EXTRA_BUILD} -D "BUILD_GUID=be4936a8-d418-405c-9f5c-a61723884a40" -D "BUILD_NAME=L4TLauncher"
+
+    PATH="${WORKSPACE}:${BTOOLS_PATH}:$PATH" \
+    build \
+       --arch "${EDK2_ARCH}" \
+       --buildtarget ${EDK2_BUILD_MODE} \
+       --tagname ${EDK_COMPILER} \
+       --platform ${S_EDK2_NVIDIA}/Platform/NVIDIA/DeviceTree/DeviceTree.dsc \
+       ${@oe.utils.parallel_make_argument(d, "-n %d")} \
+       ${EDK2_EXTRA_BUILD} -D "BUILD_GUID=4a17d121-7753-4341-b4e4-009550283be0" -D "BUILD_NAME=DeviceTree"
+
     rm -rf ${B}/images
     mkdir ${B}/images
+
     ${PYTHON} ${S_EDK2_NVIDIA}/Silicon/NVIDIA/edk2nv/FormatUefiBinary.py \
         ${B}/Build/${EDK2_PLATFORM}/${EDK2_BUILD_MODE}_${EDK_COMPILER}/FV/UEFI_NS.Fv \
         ${B}/images/${EDK2_BIN_NAME}.tmp
     mv ${B}/images/${EDK2_BIN_NAME}.tmp ${B}/images/${EDK2_BIN_NAME}
-    if ${@'false' if bb.utils.to_boolean(d.getVar('TEGRA_UEFI_MINIMAL')) else 'true'}; then
-        ${PYTHON} ${S_EDK2_NVIDIA}/Silicon/NVIDIA/edk2nv/FormatUefiBinary.py \
-                  ${B}/Build/${EDK2_PLATFORM}/${EDK2_BUILD_MODE}_${EDK_COMPILER}/AARCH64/L4TLauncher.efi \
-                  ${B}/images/BOOTAA64.efi
-    fi
-    for f in ${B}/Build/${EDK2_PLATFORM}/${EDK2_BUILD_MODE}_${EDK_COMPILER}/AARCH64/Silicon/NVIDIA/Tegra/DeviceTree/DeviceTree/OUTPUT/*.dtb; do
+
+    cp ${B}/Build/L4TLauncher/${EDK2_BUILD_MODE}_${EDK_COMPILER}/AARCH64/L4TLauncher.efi ${B}/images/BOOTAA64.efi
+
+    for f in ${B}/Build/DeviceTree/${EDK2_BUILD_MODE}_${EDK_COMPILER}/AARCH64/Silicon/NVIDIA/Tegra/DeviceTree/DeviceTree/OUTPUT/*.dtb; do
         [ -e "$f" ] || continue
         fbase=$(basename "$f" ".dtb")
         cp $f ${B}/images/$fbase.dtbo
@@ -66,9 +87,7 @@ addtask sign_efi_launcher after do_compile before do_install
 
 do_install() {
     install -d ${D}${EFIDIR}
-    if ${@'false' if bb.utils.to_boolean(d.getVar('TEGRA_UEFI_MINIMAL')) else 'true'}; then
-        install -m 0644 ${B}/images/BOOTAA64.efi ${D}${EFIDIR}/${EFI_BOOT_IMAGE}
-    fi
+    install -m 0644 ${B}/images/BOOTAA64.efi ${D}${EFIDIR}/${EFI_BOOT_IMAGE}
 }
 
 PACKAGES = "l4t-launcher"
@@ -77,7 +96,7 @@ INSANE_SKIP:l4t-launcher = "buildpaths"
 
 do_deploy() {
     install -d ${DEPLOYDIR}
-    install -m 0644 ${B}/images/${EDK2_BIN_NAME} ${DEPLOYDIR}/
+    install -m 0644 ${B}/images/${EDK2_BIN_NAME} ${DEPLOYDIR}/${TEGRA_FLASHVAR_UEFI_IMAGE}.bin
     for dtb in ${TEGRA_BOOTCONTROL_OVERLAYS} L4TConfiguration-rcmboot.dtbo; do
 	[ -e ${B}/images/$dtb ] || continue
 	install -m 0644 ${B}/images/$dtb ${DEPLOYDIR}/

OE4T/meta-tegra

recipes-bsp/uefi/edk2-firmware-tegra-rcmboot_36.5.0.bb
@@ -1,20 +1,22 @@
-require edk2-firmware-tegra-36.4.4.inc
+require edk2-firmware-tegra-36.5.0.inc
 
-DESCRIPTION = "UEFI EDK2 Minimal Firmware for Jetson platforms"
+DESCRIPTION = "UEFI EDK2 Minimal Firmware for flashing Jetson platforms"
 
 TEGRA_UEFI_SIGNING_CLASS ??= "tegra-uefi-signing"
 
 inherit l4t_bsp deploy ${TEGRA_UEFI_SIGNING_CLASS}
 
-EDK2_PLATFORM = "JetsonMinimal"
-TEGRA_UEFI_MINIMAL = "1"
 EDK2_PLATFORM_DSC = "Platform/NVIDIA/NVIDIA.common.dsc"
-EDK2_BIN_NAME = "uefi_jetson_minimal.bin"
+TEGRA_EDK2_PLATFORM ??= "UNKNOWN"
+TEGRA_EDK2_CONFIGURATION = "embedded"
+EDK2_PLATFORM = "${TEGRA_EDK2_PLATFORM}_${TEGRA_EDK2_CONFIGURATION}"
+TEGRA_FLASHVAR_UEFI_IMAGE ??= "uefi_${EDK2_PLATFORM}"
+EDK2_BIN_NAME = "uefi_${EDK2_PLATFORM}.bin"
 
 SRC_URI += "file://nvbuildconfig.py"
 
 do_configure:append() {
-    ${PYTHON} ${WORKDIR}/nvbuildconfig.py --kconfig-path=${S_EDK2_NVIDIA}/Platform/NVIDIA/Kconfig --output-dir=${B}/nvidia-config/Jetson ${S_EDK2_NVIDIA}/Platform/NVIDIA/${EDK2_PLATFORM}/Jetson.defconfig ${@config_fragments(d)}
+    ${PYTHON} ${WORKDIR}/nvbuildconfig.py --kconfig-path=${S_EDK2_NVIDIA}/Platform/NVIDIA/Kconfig --output-dir=${B}/nvidia-config/Tegra/${EDK2_PLATFORM} ${S_EDK2_NVIDIA}/Platform/NVIDIA/Tegra/DefConfigs/${EDK2_PLATFORM}.defconfig ${@config_fragments(d)}
 }
 
 do_compile:append() {
@@ -32,7 +34,7 @@ do_install() {
 
 do_deploy() {
     install -d ${DEPLOYDIR}
-    install -m 0644 ${B}/images/${EDK2_BIN_NAME} ${DEPLOYDIR}/
+    install -m 0644 ${B}/images/${EDK2_BIN_NAME} ${DEPLOYDIR}/${TEGRA_FLASHVAR_RCM_UEFI_IMAGE}.bin
 }
 # Downstream consumers will need the dtb overlays created by the
 # normal build

recipes-bsp/uefi/edk2-firmware-tegra_36.5.0.bb
@@ -1,4 +1,4 @@
-require edk2-firmware-tegra-36.4.4.inc
+require edk2-firmware-tegra-36.5.0.inc
 
 DESCRIPTION = "UEFI EDK2 Firmware for Jetson platforms"
 
@@ -10,14 +10,17 @@ TEGRA_UEFI_SIGNING_CLASS ??= "tegra-uefi-signing"
 
 inherit l4t_bsp deploy ${TEGRA_UEFI_SIGNING_CLASS}
 
-EDK2_PLATFORM = "${@'JetsonMinimal' if bb.utils.to_boolean(d.getVar('TEGRA_UEFI_MINIMAL')) else 'Jetson'}"
 EDK2_PLATFORM_DSC = "Platform/NVIDIA/NVIDIA.common.dsc"
-EDK2_BIN_NAME = "uefi_jetson.bin"
+TEGRA_EDK2_PLATFORM ??= "UNKNOWN"
+TEGRA_EDK2_CONFIGURATION ??= "general"
+EDK2_PLATFORM = "${TEGRA_EDK2_PLATFORM}_${TEGRA_EDK2_CONFIGURATION}"
+TEGRA_FLASHVAR_UEFI_IMAGE ??= "uefi_${EDK2_PLATFORM}"
+EDK2_BIN_NAME = "uefi_${EDK2_PLATFORM}.bin"
 
 SRC_URI += "file://nvbuildconfig.py"
 
 do_configure:append() {
-    ${PYTHON} ${WORKDIR}/nvbuildconfig.py --kconfig-path=${S_EDK2_NVIDIA}/Platform/NVIDIA/Kconfig --output-dir=${B}/nvidia-config/Jetson ${S_EDK2_NVIDIA}/Platform/NVIDIA/${EDK2_PLATFORM}/Jetson.defconfig ${@config_fragments(d)}
+    ${PYTHON} ${WORKDIR}/nvbuildconfig.py --kconfig-path=${S_EDK2_NVIDIA}/Platform/NVIDIA/Kconfig --output-dir=${B}/nvidia-config/Tegra/${EDK2_PLATFORM} ${S_EDK2_NVIDIA}/Platform/NVIDIA/Tegra/DefConfigs/${EDK2_PLATFORM}.defconfig ${@config_fragments(d)}
 }
 
 def fmp_lowest_version(d):
@@ -30,18 +33,36 @@ def fmp_lowest_version(d):
     return "0x%02x%02x%02x%02x" % (branch_high, branch_low, major, minor)
 
 do_compile:append() {
+
+    PATH="${WORKSPACE}:${BTOOLS_PATH}:$PATH" \
+    build \
+       --arch "${EDK2_ARCH}" \
+       --buildtarget ${EDK2_BUILD_MODE} \
+       --tagname ${EDK_COMPILER} \
+       --platform ${S_EDK2_NVIDIA}/Platform/NVIDIA/L4TLauncher/L4TLauncher.dsc \
+       ${@oe.utils.parallel_make_argument(d, "-n %d")} \
+       ${EDK2_EXTRA_BUILD} -D "BUILD_GUID=be4936a8-d418-405c-9f5c-a61723884a40" -D "BUILD_NAME=L4TLauncher"
+
+    PATH="${WORKSPACE}:${BTOOLS_PATH}:$PATH" \
+    build \
+       --arch "${EDK2_ARCH}" \
+       --buildtarget ${EDK2_BUILD_MODE} \
+       --tagname ${EDK_COMPILER} \
+       --platform ${S_EDK2_NVIDIA}/Platform/NVIDIA/DeviceTree/DeviceTree.dsc \
+       ${@oe.utils.parallel_make_argument(d, "-n %d")} \
+       ${EDK2_EXTRA_BUILD} -D "BUILD_GUID=4a17d121-7753-4341-b4e4-009550283be0" -D "BUILD_NAME=DeviceTree"
+
     rm -rf ${B}/images
     mkdir ${B}/images
+
     ${PYTHON} ${S_EDK2_NVIDIA}/Silicon/NVIDIA/edk2nv/FormatUefiBinary.py \
         ${B}/Build/${EDK2_PLATFORM}/${EDK2_BUILD_MODE}_${EDK_COMPILER}/FV/UEFI_NS.Fv \
         ${B}/images/${EDK2_BIN_NAME}.tmp
     mv ${B}/images/${EDK2_BIN_NAME}.tmp ${B}/images/${EDK2_BIN_NAME}
-    if ${@'false' if bb.utils.to_boolean(d.getVar('TEGRA_UEFI_MINIMAL')) else 'true'}; then
-        ${PYTHON} ${S_EDK2_NVIDIA}/Silicon/NVIDIA/edk2nv/FormatUefiBinary.py \
-                  ${B}/Build/${EDK2_PLATFORM}/${EDK2_BUILD_MODE}_${EDK_COMPILER}/AARCH64/L4TLauncher.efi \
-                  ${B}/images/BOOTAA64.efi
-    fi
-    for f in ${B}/Build/${EDK2_PLATFORM}/${EDK2_BUILD_MODE}_${EDK_COMPILER}/AARCH64/Silicon/NVIDIA/Tegra/DeviceTree/DeviceTree/OUTPUT/*.dtb; do
+
+    cp ${B}/Build/L4TLauncher/${EDK2_BUILD_MODE}_${EDK_COMPILER}/AARCH64/L4TLauncher.efi ${B}/images/BOOTAA64.efi
+
+    for f in ${B}/Build/DeviceTree/${EDK2_BUILD_MODE}_${EDK_COMPILER}/AARCH64/Silicon/NVIDIA/Tegra/DeviceTree/DeviceTree/OUTPUT/*.dtb; do
         [ -e "$f" ] || continue
         fbase=$(basename "$f" ".dtb")
         cp $f ${B}/images/$fbase.dtbo
@@ -66,9 +87,7 @@ addtask sign_efi_launcher after do_compile before do_install
 
 do_install() {
     install -d ${D}${EFIDIR}
-    if ${@'false' if bb.utils.to_boolean(d.getVar('TEGRA_UEFI_MINIMAL')) else 'true'}; then
-        install -m 0644 ${B}/images/BOOTAA64.efi ${D}${EFIDIR}/${EFI_BOOT_IMAGE}
-    fi
+    install -m 0644 ${B}/images/BOOTAA64.efi ${D}${EFIDIR}/${EFI_BOOT_IMAGE}
 }
 
 PACKAGES = "l4t-launcher"
@@ -77,7 +96,7 @@ INSANE_SKIP:l4t-launcher = "buildpaths"
 
 do_deploy() {
     install -d ${DEPLOYDIR}
-    install -m 0644 ${B}/images/${EDK2_BIN_NAME} ${DEPLOYDIR}/
+    install -m 0644 ${B}/images/${EDK2_BIN_NAME} ${DEPLOYDIR}/${TEGRA_FLASHVAR_UEFI_IMAGE}.bin
     for dtb in ${TEGRA_BOOTCONTROL_OVERLAYS} L4TConfiguration-rcmboot.dtbo; do
 	[ -e ${B}/images/$dtb ] || continue
 	install -m 0644 ${B}/images/$dtb ${DEPLOYDIR}/