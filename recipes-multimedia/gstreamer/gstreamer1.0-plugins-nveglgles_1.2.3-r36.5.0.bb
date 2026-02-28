madisongh/meta-tegra-wip

meta: update L4T R36.4.4 -> R36.5.0
* The usual recipe renaming and L4T version number and sha256sum
  updates
* LIC_FILES_CHKSUM updates (actual licenses still proprietary)
* Kernel args updates
* Refresh and renumber script patches

Signed-off-by: Matt Madison <matt@madison.systems>

recipes-multimedia/gstreamer/gstreamer1.0-plugins-nveglgles_1.2.3-r36.5.0.bb
@@ -1,0 +1,39 @@
+SUMMARY = "NVIDIA EGL/GLES GStreamer plugin"
+SECTION = "multimedia"
+LICENSE = "LGPL-2.0-or-later & MIT"
+LIC_FILES_CHKSUM = "file://gst-libs/gst/egl/LICENSE.libgstnvegl-1.0;md5=de0f9dfa389a77a904a5a2919a9e6b08 \
+                    file://LICENSE.libgstnveglglessink;md5=5cf2b0235eb3cb8f4073a66ecb29212a"
+
+TEGRA_SRC_SUBARCHIVE = "Linux_for_Tegra/source/gstegl_src.tbz2"
+
+require recipes-bsp/tegra-sources/tegra-sources-36.5.0.inc
+
+SRC_URI += "\
+    file://0001-Makefile-fixes-for-OE-builds.patch;patchdir=.. \
+    file://0002-Fix-builds-without-x11.patch;patchdir=.. \
+    file://0003-Fix-builds-without-wayland.patch;patchdir=.. \
+    file://0004-Fix-builds-without-wayland-IVI-extensions.patch;patchdir=.. \
+    file://0005-Convert-from-wl-shell-to-xdg-shell-for-wayland.patch;patchdir=.. \
+"
+
+DEPENDS = "tegra-mmapi gstreamer1.0 glib-2.0-native gstreamer1.0-plugins-base virtual/egl virtual/libgles2 cuda-cudart cuda-driver"
+
+PACKAGECONFIG ??= "${@bb.utils.filter('DISTRO_FEATURES', 'x11 wayland', d)}"
+PACKAGECONFIG[x11] = "USE_X11=yes,,libx11"
+PACKAGECONFIG[wayland] = "USE_WAYLAND=yes,,wayland wayland-protocols wayland-native"
+
+EXTRA_OEMAKE = "CUDA_VER=${CUDA_VERSION} ${PACKAGECONFIG_CONFARGS}"
+
+CFLAGS += "-Wno-error=int-conversion"
+
+S = "${UNPACKDIR}/gstegl_src/gst-egl"
+
+inherit pkgconfig features_check
+
+REQUIRED_DISTRO_FEATURES = "opengl"
+
+do_install() {
+    oe_runmake install DESTDIR="${D}"
+}
+
+FILES:${PN} = "${libdir}/gstreamer-1.0"

recipes-multimedia/gstreamer/gstreamer1.0-plugins-nvtee_1.14.0-r36.5.0.bb
@@ -1,0 +1,20 @@
+DESCRIPTION = "NVIDIA video tee GStreamer plugin"
+SECTION = "multimedia"
+LICENSE = "BSD-3-Clause"
+LIC_FILES_CHKSUM = "file://README.txt;endline=26;md5=d4da79f8cebc6b73ce481b090afa99ae"
+
+TEGRA_SRC_SUBARCHIVE = "Linux_for_Tegra/source/gst-nvtee_src.tbz2"
+require recipes-bsp/tegra-sources/tegra-sources-36.5.0.inc
+
+DEPENDS = "gstreamer1.0 glib-2.0 gstreamer1.0-plugins-base"
+
+SRC_URI += " file://0001-Update-makefile-for-OE-builds.patch"
+
+S = "${UNPACKDIR}/gst-nvtee"
+
+inherit pkgconfig
+
+do_install() {
+	oe_runmake install DESTDIR="${D}"
+}
+FILES:${PN} = "${libdir}/gstreamer-1.0"

recipes-multimedia/gstreamer/gstreamer1.0-plugins-nvv4l2camerasrc_1.14.0-r36.5.0.bb
@@ -1,0 +1,31 @@
+DESCRIPTION = "NVIDIA v4l2camerasrc GStreamer plugin"
+SECTION = "multimedia"
+LICENSE = "BSD-3-Clause & Proprietary"
+LIC_FILES_CHKSUM = "file://nvbufsurface.h;endline=9;md5=ba9f51385aebf755c973fc0a6b2277ad \
+                    file://README.txt;endline=25;md5=afc286435ccd143c9a10b5d7a8c1dee1 \
+"
+
+TEGRA_SRC_SUBARCHIVE = "Linux_for_Tegra/source/gst-nvv4l2camera_src.tbz2"
+TEGRA_SRC_SUBARCHIVE_OPTS = "--exclude=3rdpartyheaders.tbz2"
+TEGRA_SRC_EXTRA_SUBARCHIVE = "Linux_for_Tegra/source/gst-nvarguscamera_src.tbz2"
+TEGRA_SRC_EXTRA_SUBARCHIVE_OPTS = "-C ${UNPACKDIR}/gst-nvv4l2camera --strip-components=1 gst-nvarguscamera/nvbufsurface.h"
+
+require recipes-bsp/tegra-sources/tegra-sources-36.5.0.inc
+
+SRC_URI += "\
+    file://0001-Build-fixups.patch \
+    file://0002-Clean-up-compiler-warnings.patch \
+"
+
+DEPENDS = "gstreamer1.0 glib-2.0 gstreamer1.0-plugins-base gstreamer1.0-plugins-good libv4l virtual/egl tegra-libraries-multimedia"
+
+S = "${UNPACKDIR}/gst-nvv4l2camera"
+
+inherit pkgconfig features_check
+
+REQUIRED_DISTRO_FEATURES = "opengl"
+
+do_install() {
+	oe_runmake install DESTDIR="${D}"
+}
+FILES:${PN} = "${libdir}/gstreamer-1.0"

OE4T/meta-tegra

recipes-multimedia/gstreamer/gstreamer1.0-plugins-nveglgles_1.2.3-r36.5.0.bb
@@ -1,0 +1,37 @@
+SUMMARY = "NVIDIA EGL/GLES GStreamer plugin"
+SECTION = "multimedia"
+LICENSE = "LGPL-2.0-or-later & MIT"
+LIC_FILES_CHKSUM = "file://gst-libs/gst/egl/LICENSE.libgstnvegl-1.0;md5=de0f9dfa389a77a904a5a2919a9e6b08 \
+                    file://LICENSE.libgstnveglglessink;md5=5cf2b0235eb3cb8f4073a66ecb29212a"
+
+TEGRA_SRC_SUBARCHIVE = "Linux_for_Tegra/source/gstegl_src.tbz2"
+
+require recipes-bsp/tegra-sources/tegra-sources-36.5.0.inc
+
+SRC_URI += "\
+    file://0001-Makefile-fixes-for-OE-builds.patch;patchdir=.. \
+    file://0002-Fix-builds-without-x11.patch;patchdir=.. \
+    file://0003-Fix-builds-without-wayland.patch;patchdir=.. \
+    file://0004-Fix-builds-without-wayland-IVI-extensions.patch;patchdir=.. \
+    file://0005-Convert-from-wl-shell-to-xdg-shell-for-wayland.patch;patchdir=.. \
+"
+
+DEPENDS = "tegra-mmapi gstreamer1.0 glib-2.0-native gstreamer1.0-plugins-base virtual/egl virtual/libgles2 cuda-cudart cuda-driver"
+
+PACKAGECONFIG ??= "${@bb.utils.filter('DISTRO_FEATURES', 'x11 wayland', d)}"
+PACKAGECONFIG[x11] = "USE_X11=yes,,libx11"
+PACKAGECONFIG[wayland] = "USE_WAYLAND=yes,,wayland wayland-protocols wayland-native"
+
+EXTRA_OEMAKE = "CUDA_VER=${CUDA_VERSION} ${PACKAGECONFIG_CONFARGS}"
+
+S = "${WORKDIR}/gstegl_src/gst-egl"
+
+inherit pkgconfig features_check
+
+REQUIRED_DISTRO_FEATURES = "opengl"
+
+do_install() {
+    oe_runmake install DESTDIR="${D}"
+}
+
+FILES:${PN} = "${libdir}/gstreamer-1.0"

recipes-multimedia/gstreamer/gstreamer1.0-plugins-nvtee_1.14.0-r36.5.0.bb
@@ -1,0 +1,20 @@
+DESCRIPTION = "NVIDIA video tee GStreamer plugin"
+SECTION = "multimedia"
+LICENSE = "BSD-3-Clause"
+LIC_FILES_CHKSUM = "file://README.txt;endline=26;md5=d4da79f8cebc6b73ce481b090afa99ae"
+
+TEGRA_SRC_SUBARCHIVE = "Linux_for_Tegra/source/gst-nvtee_src.tbz2"
+require recipes-bsp/tegra-sources/tegra-sources-36.5.0.inc
+
+DEPENDS = "gstreamer1.0 glib-2.0 gstreamer1.0-plugins-base"
+
+SRC_URI += " file://0001-Update-makefile-for-OE-builds.patch"
+
+S = "${WORKDIR}/gst-nvtee"
+
+inherit pkgconfig
+
+do_install() {
+	oe_runmake install DESTDIR="${D}"
+}
+FILES:${PN} = "${libdir}/gstreamer-1.0"

recipes-multimedia/gstreamer/gstreamer1.0-plugins-nvv4l2camerasrc_1.14.0-r36.5.0.bb
@@ -1,0 +1,31 @@
+DESCRIPTION = "NVIDIA v4l2camerasrc GStreamer plugin"
+SECTION = "multimedia"
+LICENSE = "BSD-3-Clause & Proprietary"
+LIC_FILES_CHKSUM = "file://nvbufsurface.h;endline=9;md5=ba9f51385aebf755c973fc0a6b2277ad \
+                    file://README.txt;endline=25;md5=afc286435ccd143c9a10b5d7a8c1dee1 \
+"
+
+TEGRA_SRC_SUBARCHIVE = "Linux_for_Tegra/source/gst-nvv4l2camera_src.tbz2"
+TEGRA_SRC_SUBARCHIVE_OPTS = "--exclude=3rdpartyheaders.tbz2"
+TEGRA_SRC_EXTRA_SUBARCHIVE = "Linux_for_Tegra/source/gst-nvarguscamera_src.tbz2"
+TEGRA_SRC_EXTRA_SUBARCHIVE_OPTS = "-C ${S} --strip-components=1 gst-nvarguscamera/nvbufsurface.h"
+
+require recipes-bsp/tegra-sources/tegra-sources-36.5.0.inc
+
+SRC_URI += "\
+    file://0001-Build-fixups.patch \
+    file://0002-Clean-up-compiler-warnings.patch \
+"
+
+DEPENDS = "gstreamer1.0 glib-2.0 gstreamer1.0-plugins-base gstreamer1.0-plugins-good libv4l virtual/egl tegra-libraries-multimedia"
+
+S = "${WORKDIR}/gst-nvv4l2camera"
+
+inherit pkgconfig features_check
+
+REQUIRED_DISTRO_FEATURES = "opengl"
+
+do_install() {
+	oe_runmake install DESTDIR="${D}"
+}
+FILES:${PN} = "${libdir}/gstreamer-1.0"