51fb5fdcbb0becc23084456b838fe9ba5c93fba5
Matt Madison <matt@madison.systems>
2026-07-24 13:07:04 -0700

    lib: add uefi functions for minimal boot handling
    Signed-off-by: Matt Madison <matt@madison.systems>

--- lib/oe4t/uefi.py
+++ lib/oe4t/uefi.py
@@ -6,3 +6,19 @@ def get_hex_version(l4t_version):
     major = int(verparts[1]) & 0xff
     minor = int(verparts[2]) & 0xff
     return "0x%02x%02x%02x%02x" % (branch_high, branch_low, major, minor)
+
+def derive_config_name(d):
+    if not bb.utils.to_boolean(d.getVar('TEGRA_MINIMAL_BOOT')):
+        return 'general'
+    if bb.utils.to_boolean(d.getVar('TEGRAFLASH_NO_INTERNAL_STORAGE') or '0') or d.getVar('TNSPEC_BOOTDEV').startswith('nvme'):
+        return 'simple_pcie'
+    return 'simple_emmc'
+
+def default_boot_order(d):
+    if not bb.utils.to_boolean(d.getVar('TEGRA_MINIMAL_BOOT')):
+        return ''
+    if bb.utils.to_boolean(d.getVar('TEGRAFLASH_NO_INTERNAL_STORAGE') or '0') or d.getVar('TNSPEC_BOOTDEV').startswith('nvme'):
+        return 'BootOrderNvme.dtbo'
+    if d.getVar('TNSPEC_BOOTDEV_DEFAULT') == "mmcblk1p1":
+        return 'BootOrderSD.dtbo'
+    return 'BootOrderEmmc.dtbo'
